d781683fae03bb7b85bf058a0e92f735095c0b95
Fixsbreaker <alinurkunbol@gmail.com>
2026-04-08 23:16:55 +0500

    add basic case commands up/low/cap

--- processor/processor.go
+++ processor/processor.go
@@ -1,5 +1,40 @@
 package processor
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func Process(text string) string {
-	return text
+	words := strings.Fields(text)
+	var processed []string
+
+	for _, word := range words {
+		switch word {
+		case "(up)":
+			if len(processed) > 0 {
+				processed[len(processed)-1] = strings.ToUpper(processed[len(processed)-1])
+			}
+		case "(low)":
+			if len(processed) > 0 {
+				processed[len(processed)-1] = strings.ToLower(processed[len(processed)-1])
+			}
+		case "(cap)":
+			if len(processed) > 0 {
+				processed[len(processed)-1] = capitalize(processed[len(processed)-1])
+			}
+		default:
+			processed = append(processed, word)
+		}
+	}
+
+	return strings.Join(processed, " ")
+}
+
+func capitalize(s string) string {
+	if s == "" {
+		return ""
+	}
+	r, size := utf8.DecodeRuneInString(s)
+	return strings.ToUpper(string(r)) + strings.ToLower(s[size:])
 }
